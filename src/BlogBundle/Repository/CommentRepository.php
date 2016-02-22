455f94dfe552c114685ce79a613f2278f60de749
petrero <petrero@gmail.com>
2016-02-22 22:44:28 +0200

    Create a custom query to retrieve the approved comments for a blog

--- src/BlogBundle/Repository/CommentRepository.php
+++ src/BlogBundle/Repository/CommentRepository.php
@@ -10,4 +10,17 @@ use Doctrine\ORM\EntityRepository;
  */
 class CommentRepository extends EntityRepository
 {
+  public function getCommentsForBlog($blogId, $approved = true){
+    $qb = $this->createQueryBuilder('c')
+          ->where('c.blog = :blog_id')
+          ->addOrderBy('c.created')
+          ->setParameter('blog_id', $blogId);
+    
+    if (false === is_null($approved)){
+      $qb->andWhere('c.approved = :approved')
+         ->setParameter('approved', $approved);
+    }
+    
+    return $qb->getQuery()->getResult();
+  }
 }
